3b8716329e1510dd04d080b9293df430cda1b2f1
Muhammad Zain <m.zaini.97@gmail.com>
2022-09-17 16:52:22 +0500

    Project Created

---
+++ src/test/java/com/maqsad/ewallet/EWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maqsad.ewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
