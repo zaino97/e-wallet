e3cec37d75ce33b1cdcfb4e10f5b1cdc4264d57f
Muhammad Zain <m.zaini.97@gmail.com>
2022-09-19 01:01:25 +0500

    final commit

--- src/main/java/com/maqsad/ewallet/bootstrapdata/BootStrapData.java
+++ src/main/java/com/maqsad/ewallet/bootstrapdata/BootStrapData.java
@@ -28,35 +28,6 @@ public class BootStrapData implements CommandLineRunner{
 	        	System.out.println(e.getMessage());
 	        }
 	        
-	        
-			/*
-			 * if(!walletService.deposit("50", 50)) {
-			 * System.out.println("Error: cannot add credit"); } else {
-			 * System.out.println("Credit added successfully. Your balance is: " +
-			 * walletService.getBalance("100")); } if(!walletService.makePayment("50", 30))
-			 * { System.out.println("No sufficeient balance"); } else {
-			 * System.out.println("Wallet Balance: " +walletService.getBalance("100")); }
-			 */
-	        
-	        /*
-			 * Author eric = new Author("Eric", "Evans"); Book ddd = new
-			 * Book("Domain Driven Design", "123123"); eric.getBooks().add(ddd);
-			 * ddd.getAuthors().add(eric);
-			 * 
-			 * ddd.setPublisher(publisher); publisher.getBooks().add(ddd);
-			 * 
-			 * authorRepository.save(eric); bookRepository.save(ddd);
-			 * publisherRepository.save(publisher);
-			 * 
-			 * Author rod = new Author("Rod", "Johnson"); Book noEJB = new
-			 * Book("J2EE Development without EJB", "3939459459");
-			 * rod.getBooks().add(noEJB); noEJB.getAuthors().add(rod);
-			 * 
-			 * noEJB.setPublisher(publisher); publisher.getBooks().add(noEJB);
-			 * 
-			 * authorRepository.save(rod); bookRepository.save(noEJB);
-			 * publisherRepository.save(publisher);
-			 */
 	    }
 
 }
